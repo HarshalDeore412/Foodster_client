f5a1f761d168975f34c8baac4699e1948f61167b
Harshal Deore <86178850+HarshalDeore412@users.noreply.github.com>
2024-06-21 15:53:45 +0530

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,15 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import img1 from "../assects/Images/img-1.jpg";
 import img2 from "../assects/Images/img-2.jpg";
 import img3 from "../assects/Images/img-3.jpg";
 import img4 from "../assects/Images/img-4.jpg";
 import img5 from "../assects/Images/img-5.jpg";
-import { useState , useEffect } from "react";
 
 const Carousel = () => {
-  
   const [currentIndex, setCurrentIndex] = useState(0);
-  const images = [img1, img2, img3,img4,img5]; // Replace with your image URLs
+  const images = [img1, img2, img3, img4, img5]; // Replace with your image URLs
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -27,13 +25,16 @@ const Carousel = () => {
       >
         {images.map((image, index) => (
           <div key={index} className="carousel-item flex-none w-full">
-            <img className="index-0" className="h-400 w-full" src={image} alt={`Image ${index + 1}`} />
+            <img
+              className="index-0 h-[400px] w-full object-cover"
+              src={image}
+              alt={`Image ${index + 1}`}
+            />
           </div>
         ))}
       </div>
     </div>
   );
 };
-
 
 export default Carousel;
