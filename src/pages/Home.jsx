21ecfb27baaca71035660265272b7b6a94b87f98
Harshal Deore <86178850+HarshalDeore412@users.noreply.github.com>
2024-06-26 14:11:44 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="w-screen max-w-screen-3xl" >
+    <div className="w-screen " >
       <div>
         <Navbar props={{ search, setSearch }} />
       </div>
