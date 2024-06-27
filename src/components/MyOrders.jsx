655f99c932c0af1b71af027ca975cfa33cdee869
Harshal Deore <86178850+HarshalDeore412@users.noreply.github.com>
2024-06-27 10:57:52 +0530

    Update MyOrders.jsx

--- src/components/MyOrders.jsx
+++ src/components/MyOrders.jsx
@@ -39,7 +39,7 @@ const MyOrders = () => {
         <Navbar />
       </div>
 
-      <div>
+      {/* <div>
         {orderData ? (
           <div className=" bg-gray-900 text-white ">
             {orderData.order_data.map((data, index) => {
@@ -93,7 +93,48 @@ const MyOrders = () => {
           </div>
         )}
       </div>
+*/}
 
+  {orderData ? (
+  <div className="bg-gray-900 text-white">
+    {orderData.order_data.reverse().map((data, index) => {
+      return (
+        <div key={index} className="p-5">
+          {data.map((order, index) => {
+            return (
+              <div key={index}>
+                {order.Order_date ? (
+                  <div>
+                    {order.Order_date} <hr />
+                  </div>
+                ) : (
+                  ""
+                )}
+                <div>
+                  <img
+                    className="h-[100px] w-[150px]"
+                    src={order.image}
+                    alt=""
+                  />
+                </div>
+                <div>{order.name}</div>
+                <div>{order.price}</div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    })}
+  </div>
+) : (
+  <div>
+    <div className="flex justify-center text-4xl items-center h-screen w-full">
+      <div>YOU HAVE NOT ORDERED ANYTHING YET</div>
+    </div>
+  </div>
+)}
+
+  
       <div>
         <Footer />
       </div>
