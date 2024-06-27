91fada27f1e13bf595ed9c044b4cff347fde68a6
Harshal Deore <86178850+HarshalDeore412@users.noreply.github.com>
2024-06-27 10:23:10 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import Logo from "../assects/Images/LOGO.jpg"
 const Footer = () => {
   return (
     <div>
-      <footer className="bg-white w-full fixed bottom-0 left-0 shadow dark:bg-gray-900 ">
+      <footer className="bg-white w-full absolute bottom-0 left-0 shadow dark:bg-gray-900 ">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <Link
